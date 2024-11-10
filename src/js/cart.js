86ed3e7f2a7d43c3e8c1ee78f6653759a2148521
Andrea Biason <andrea.biason5@gmail.com>
2024-11-10 17:19:21 +0100

    Cart animation and methods to add and remove items and calculate total

--- src/js/cart.js
+++ src/js/cart.js
@@ -10,6 +10,8 @@ class Cart {
     this.cartButtonNumber = document.querySelector('.cart-button__number');
     this.cartButtonBg = document.querySelector('.cart-button__number-bg');
     this.cartClose = document.querySelector('.cart__inner-close');
+    this.cartTotal = document.querySelector('.cart-total__amount');
+    this.cartItemsList = document.querySelector('.cart-items');
 
     this.cartOpened = false;
 
@@ -51,15 +53,101 @@ class Cart {
     })
   }
 
+  updateCart() {
+    const cartElementsQuantities = [...document.querySelectorAll('.cart-item__quantity')];
+    this.cartButtonNumber.innerHTML = Object.values(this.cartItems).length;
+    
+    let cartAmount = 0;
+
+    Object.values(this.cartItems).forEach((item, i) => {
+      cartElementsQuantities[i].innerHTML = item.quantity;
+      cartAmount+= item.price * item.quantity
+    })
+
+    this.cartTotal.innerHTML = `€ ${cartAmount}`;
+  }
+
+  appendItem(item) {
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('cart-item', 'cart-grid');
+    
+    cartItem.innerHTML = `
+      <img class="cart-item__img" src="${item.cover}" alt="${item.name}">
+    
+      <div class="cart-item__details">
+        <span class="cart-item__details-title">${item.name}</span>
+
+        <button class="cart-item__remove-btn">Remove</button>
+
+        <div class="cart-item__details-wrap">
+          <span class="cart-item__details-label">Quantity:</span>
+
+          <div class="cart-item__details-actions">
+            <button class="cart-item__minus-button">-</button>
+            <span class="cart-item__quantity">${item.quantity}</span>
+            <button class="cart-item__plus-button">+</button>
+          </div>
+          <span class="cart-item__details-price">€ ${item.price}</span>
+        </div>
+      </div>
+    `;
+
+    const removeButton = cartItem.querySelector('.cart-item__remove-btn');
+    const plusButton = cartItem.querySelector('.cart-item__plus-button');
+    const minusButton = cartItem.querySelector('.cart-item__minus-button');
+
+    removeButton.addEventListener('click', () => this.removeItemFromCart(item.id));
+    plusButton.addEventListener('click', () => this.updateQuantity(item.id, 1));
+    minusButton.addEventListener('click', () => this.updateQuantity(item.id, -1));
+
+    return cartItem;
+  }
+
+  addItemToCart(el) {
+    const { id, price, name, cover } = el.dataset;
+
+    const index = this.cartItems.findIndex((el) => el.id === id);
+
+    if (index < 0) {
+      const newItem = { id, price, name, cover, quantity: 1 };
+      this.cartItems.push(newItem);
+
+      const newCartItem = this.appendItem(newItem);
+      this.cartItemsList.append(newCartItem);
+    } else this.cartItems[index].quantity += 1;
+
+    this.updateCart();
+  }
+
+  updateQuantity(id, quantity) {
+    const index = this.cartItems.findIndex((el) => el.id === id);
+    const newQuantity = this.cartItems[index].quantity + quantity;
+
+    if (newQuantity > 0) {
+      this.cartItems[index].quantity = newQuantity
+      this.updateCart();
+    } else if (newQuantity === 0) {
+      this.removeItemFromCart(id);
+    }
+
+  }
+
+  removeItemFromCart(id){
+    const index = this.cartItems.findIndex((el) => el.id === id);
+
+    this.cartItems.splice(index, 1);
+
+    const cartHTMLNodes = [...document.querySelectorAll('.cart-item')];
+    cartHTMLNodes[index].remove();
+
+    this.updateCart();
+  }
+
   setupLabelAnimation() {
     gsap.set([this.cartButtonNumber, this.cartButtonBg], { scale: 0 });
   }
 
-  updateCartNumber() {
-    this.cartButtonNumber.innerHTML = this.cartItems.length;
-  }
-
-  labelAnimation() {
+  cartButtonAnimation() {
     const tl = gsap.timeline();
     tl.addLabel('start');
 
@@ -68,15 +156,6 @@ class Cart {
       scale: 1, stagger: 0.1, duration: 0.8, ease: 'elastic.out(1.3, 0.9)',
     }, 'start');
     return tl;
-  }
-
-  addItemToCart(el) {
-    const { id, price, name, cover } = el.dataset;
-
-    if (!this.cartItems[id]) this.cartItems[id] = { price, name, cover, quantity: 1 };
-    else this.cartItems[id].quantity += 1;
-
-    this.updateCartNumber();
   }
 
   cartAnimationSetup() {
@@ -95,7 +174,7 @@ class Cart {
     
     gsap.set([this.animatingElements.bg, this.animatingElements.innerBg], { xPercent: 110 });
     gsap.set(this.animatingElements.close, { x: 30, autoAlpha: 0 });
-    if (this.animatingElements.items) gsap.set(this.animatingElements.items, { x: 30, autoAlpha: 0 });
+    if (this.animatingElements.items.length > 0) gsap.set(this.animatingElements.items, { x: 30, autoAlpha: 0 });
     gsap.set(this.animatingElements.total, { scale: 0.9, autoAlpha: 0 });
   };
   
@@ -117,7 +196,7 @@ class Cart {
       x: 0, autoAlpha: 1, stagger: 0.1, duration: 1, ease: 'power2.out',
     }, 'start+=1.3');
   
-    if (this.animatingElements.items) {
+    if (this.animatingElements.items.length > 0) {
       tl.to(this.animatingElements.items, {
         x: 0, autoAlpha: 1, stagger: 0.1, duration: 1, ease: 'power2.out',
       }, 'start+=1.4');
@@ -145,7 +224,7 @@ class Cart {
       xPercent: 110, stagger: 0.1, duration: 1.5, ease: 'expo.inOut',
     }, 'start');
 
-    if (this.animatingElements.items) {
+    if (this.animatingElements.items.length > 0) {
       tl.to(this.animatingElements.items, {
         x: 30, autoAlpha: 0, stagger: 0.1, duration: 0.8, ease: 'power2.out',
       }, 'start');
