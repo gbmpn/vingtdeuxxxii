3d7dcc83cc1eb8e53801c73e520395dd15c1e661
GB M <gbm.workshop@gmail.com>
2025-02-14 18:57:07 +0100

    base layout

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,52 @@
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import glsl from 'vite-plugin-glsl';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { resolve } from 'path';
 
-export default {
-  root: 'src/',
-  publicDir: '../static/',
-  base: './',
+export default defineConfig({
+  root: 'src/', // Set the root directory for the app
+  publicDir: '../static/', // Directory for static assets
+  base: './', // Base path for the app
+
   server: {
-    host: true,
-    open: true,
+    host: true, // Enable access from network devices
+    open: true, // Open browser on server start
+    port: 8000, // Port for the dev server
   },
+
+  build: {
+    outDir: '../dist', // Output directory for build files
+    emptyOutDir: true, // Clean the output directory before building
+    sourcemap: true, // Generate source maps
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/index.html'), // Entry point for the app
+      },
+    },
+  },
+
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src/js'), // Alias for JS directory
+    },
+  },
+
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern'
-      }
-    }
-  },
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, 'src/index.html'),
+        api: 'modern', // SCSS options
       },
     },
-  }
-}
+  },
+
+  assetsInclude: ['**/*.fnt'], // Include font files as assets
+
+  plugins: [
+    createHtmlPlugin({
+      minify: true, // Minify the HTML
+    }),
+    glsl(), // Handle GLSL shaders
+    nodePolyfills(), // Polyfills for Node.js APIs
+  ],
+});
